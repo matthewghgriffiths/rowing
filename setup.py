e16640cb86adb230baa6cecc186b3baca1efb82b
Matthew Griffiths <matthew@concr.co>
2021-06-14 12:56:31 +0100

    updating to v0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='world_rowing',
-    version='0.1.0',
+    version='0.1.1',
     description='Library for loading and presenting data from the worldrowing.com',
     author='Matthew Grifiths',
     author_email='matthewghgriffiths@gmail.com',
