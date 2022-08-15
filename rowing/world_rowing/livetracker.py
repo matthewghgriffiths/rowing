190dc67b5214fb72644cdc98b622d31693c27b56
Matthew Griffiths <matthewghgriffiths@gmail.com>
2022-08-15 20:09:53 +0100

    some bugfixes for cli

--- rowing/world_rowing/livetracker.py
+++ rowing/world_rowing/livetracker.py
@@ -17,7 +17,8 @@ from .api import (
 )
 from .utils import (
     extract_fields, format_yaxis_splits, make_flag_box, update_fill_betweenx,
-    update_fill_between, read_times, format_totalseconds, cached_property
+    update_fill_between, read_times, format_totalseconds, cached_property,
+    format_timedelta
 )
 
 logger = logging.getLogger("world_rowing.livetracker")
@@ -484,12 +485,10 @@ class RaceTracker:
 
 def get_current_data(live_data):
     current_data = live_data.iloc[[-1]].copy()
-    current_data.PGMT = current_data.PGMT.applymap("{:.1%}".format)
-    current_data.time = current_data.time.apply(format_totalseconds)
-    current_data.GMT = current_data.GMT.applymap(format_totalseconds)
-    current = current_data.set_index("time").astype('string').T.unstack(1)
-    current.columns.names = ['time elapsed', 'Country']
-    return current
+    current_data.PGMT = current_data.PGMT.applymap("{:.1%}".format)    
+    current_data['time elapsed'] = current_data.time.max(1).map(utils.format_totalseconds)
+    current_data.time = current_data.time.applymap(utils.format_totalseconds)
+    return current_data.set_index('time elapsed').astype('string').T.unstack(1)
 
 
 def get_race_livetracker(race_id, gmt=None, cached=True):
