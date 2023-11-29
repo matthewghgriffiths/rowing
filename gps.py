c27f4d9a433781678dc89ed2cb3b6fea99561cd2
Matthew Griffiths <matthewghgriffiths@gmail.com>
2023-11-29 22:34:38 +0000

    updating gps.py with piece selecter

--- gps.py
+++ gps.py
@@ -1,11 +1,10 @@
 import streamlit as st
+import io 
 
 import logging 
 
-import numpy as np 
+import numpy as np
 import pandas as pd 
-
-from tqdm.auto import tqdm
 
 from rowing.analysis import files, splits, geodesy
 from rowing import utils
@@ -13,11 +12,11 @@ from rowing import utils
 logger = logging.getLogger(__name__)
 
 st.set_page_config(
-    page_title="GPS",
+    page_title="GPX",
     layout='wide'
 )
 """
-# GPS data processing
+# GPX data processing
 """
 
 uploaded_files = st.file_uploader(
@@ -27,7 +26,7 @@ uploaded_files = st.file_uploader(
 def parse_gpx(file):
     return files.parse_gpx_data(files.gpxpy.parse(file))
 
-def download_csv(file_name, df, label="Download data as CSV", **kwargs):
+def download_csv(file_name, df, label=":inbox_tray: Download data as csv", **kwargs):
     st.download_button(
         label=label, 
         file_name=file_name,
@@ -45,6 +44,10 @@ gpx_data, errors = utils.map_concurrent(
 if not gpx_data:
     st.write("No data uploaded")
     st.stop()
+
+
+
+
 
 with st.spinner("Processing Crossing Times"):
     crossing_times, errors = utils.map_concurrent(
@@ -90,6 +93,103 @@ with st.expander("Individual Crossing Times"):
             download_csv(f"{name}-crossings.csv", show_crossings)
 
 
+with st.expander("Piece selecter"):
+    piece_dates = np.sort(all_crossing_times.dt.date.unique())
+
+    cols = st.columns(3)
+    with cols[0]:
+        select_date = st.selectbox(
+            "select piece date", 
+            piece_dates, 
+            index=piece_dates.size - 1, 
+        )
+    sel_times = all_crossing_times[
+        all_crossing_times.dt.date == select_date
+    ]
+    landmarks = sel_times.index.levels[3]
+    with cols[1]:
+        start_landmark = st.selectbox(
+            "select start landmark", 
+            landmarks, 
+        )
+    with cols[2]:
+        finish_landmark = st.selectbox(
+            "select finish landmark", 
+            landmarks, 
+        )
+
+    start_times = sel_times.xs(start_landmark, level=3).droplevel(-1)
+    finish_times = sel_times.xs(finish_landmark, level=3).droplevel(-1)
+    times = pd.concat({
+        "Elapsed time": sel_times - start_times,
+        "Time left": finish_times - sel_times,
+        "Time": sel_times, 
+    }, axis=1)
+    piece_data = times[
+        times.notna().all(axis=1)
+        & (times['Time left'].dt.total_seconds() >= 0)
+        & (times["Elapsed time"].dt.total_seconds() >= 0)
+    ].reset_index("distance").unstack()
+    legs = piece_data.index
+    startfinish = pd.concat({
+        "Start time": start_times.loc[legs], 
+        "Finish Time": finish_times.loc[legs]
+    }, axis=1)
+    piece_data.index = pd.MultiIndex.from_frame(
+        start_times.loc[legs].rename("Start Time").reset_index()[
+            ["Start Time", "file", "location", "leg"]
+        ]
+    )
+    piece_data = piece_data.sort_index(level=0)
+    legs = piece_data.index.droplevel(0)
+
+    piece_distances = (
+        piece_data.distance 
+        - piece_data.distance[start_landmark].values[:, None]
+    )
+    piece_time = piece_data['Elapsed time']
+    avg_split = (piece_time * 0.5 / piece_distances).fillna(pd.Timedelta(0))
+    interval_split = (
+        piece_time.diff(axis=1) * 0.5 / piece_distances.diff(axis=1)
+    ).fillna(pd.Timedelta(0))
+    
+    st.dataframe(
+        startfinish
+    )
+    tabs = st.tabs([
+        "Elapsed Time", 
+        "Distance Travelled", 
+        "Average Split", 
+        "Interval Split", 
+        "Timestamp"
+    ])
+    with tabs[0]:
+        st.dataframe(
+            piece_time.applymap(
+                utils.format_timedelta, #hours=True
+            )
+        )
+    with tabs[1]:
+        st.dataframe(
+            piece_distances
+        )
+
+    with tabs[2]:
+        st.dataframe(
+            interval_split.applymap(
+                utils.format_timedelta, #hours=True
+            )
+        )
+
+    with tabs[3]:
+        st.dataframe(
+            avg_split.applymap(
+                utils.format_timedelta, #hours=True
+            )
+        )
+    with tabs[4]:
+        st.dataframe(piece_data['Time'])
+
 with st.spinner("Processing split timings"):
     location_timings, errors = utils.map_concurrent(
         splits.get_location_timings, gpx_data, singleton=True
@@ -114,3 +214,67 @@ with st.expander("Landmark timings"):
                 mime="text/csv",
             )
 
+with st.spinner("Processing fastest times"):
+    best_times, errors = utils.map_concurrent(
+        splits.find_all_best_times, 
+        gpx_data, singleton=True, 
+    )
+
+with st.expander("Fastest times"):
+    tabs = st.tabs(best_times)
+    for tab, (name, times) in zip(tabs, best_times.items()):
+        show_times = times + pd.Timestamp(0)
+        print(times.dtypes)
+        with tab:
+            st.dataframe(
+                show_times, 
+                column_config={
+                    "split": st.column_config.TimeColumn(
+                        "Split", format="m:ss.SS"
+                    ),
+                    "time": st.column_config.TimeColumn(
+                        "Time", format="m:ss.SS"
+                    )
+                }
+            )
+            download_csv(
+                f"{name}-fastest.csv", 
+                times.applymap(
+                    utils.format_timedelta, hours=True
+                ).replace("00:00:00.00", "")
+            )
+
+with st.spinner("Generating excel file"):
+    xldata = io.BytesIO()
+    with pd.ExcelWriter(xldata) as xlf:
+        for name, crossings in crossing_times.items():
+            crossings = crossings.rename("time").dt.tz_localize(None)
+            crossings.to_frame().to_excel(
+                xlf, f"{name}-crossings"
+            )
+
+        for name, timings in location_timings.items():
+            timings.index.names = "", 'leg', 'landmark', 'distance'
+            timings.columns.names = "", 'leg', 'landmark', 'distance'
+            upload_timings = timings.applymap(
+                utils.format_timedelta, hours=True
+            ).replace("00:00:00.00", "").T
+
+            upload_timings.to_excel(
+                xlf, f"{name}-timings"
+            )
+
+        for name, times in best_times.items():
+            times.applymap(
+                utils.format_timedelta, hours=True
+            ).replace("00:00:00.00", "").to_excel(
+                xlf, f"{name}-fastest"
+            )
+
+    xldata.seek(0)
+    st.download_button(
+        ":inbox_tray: Download all: GPS_data.xlsx", 
+        xldata,
+        # type='primary', 
+        file_name="GPS_data.xlsx",  
+    )
